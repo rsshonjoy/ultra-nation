45859ac3b2c0407f7101448390eade025c2ac619
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-27 01:36:30 +0600

    display country, capital and population

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Country = (props) => {
-  const { countryName } = props;
+  const { country } = props;
   console.log(props);
   return (
     <div>
-      <h4>This is a {countryName}</h4>
+      <h4>This is a {country.name}</h4>
+      <p>Capital: {country.capital}</p>
+      <p>Population: {country.population}</p>
     </div>
   );
 };
