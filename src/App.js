ccb4d7c20f8a9d922ab9484a5121e5f310dbff58
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-27 02:31:39 +0600

    add flag

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
       .then((res) => res.json())
       .then((data) => {
         setCountries(data);
-        console.log(data);
-        const names = data.map((country) => country.name);
-        console.log(names);
+        // console.log(data);
+        // const names = data.map((country) => country.name);
+        // console.log(names);
       })
       .catch((error) => console.log(error));
   }, []);
@@ -19,7 +19,7 @@ function App() {
     <div className="App">
       <h1>Country Loaded: {countries.length}</h1>
       {countries.map((country) => (
-        <Country country={country} />
+        <Country country={country} key={country.alpha3Code} />
       ))}
     </div>
   );
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 const Country = (props) => {
   const { country } = props;
-  console.log(props);
+  const flagStyle = { height: '50px' };
   return (
     <div>
       <h4>This is a {country.name}</h4>
+      <img style={flagStyle} src={country.flag} alt="" />
       <p>Capital: {country.capital}</p>
       <p>Population: {country.population}</p>
+      <p>
+        <small>Region: {country.region}</small>
+      </p>
     </div>
   );
 };
