650bb5a645382c264590df1d7ef0801d9da47d86
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-26 23:49:09 +0600

    JSON error handle

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   useEffect(() => {
     fetch('https://restcountries.eu/rest/v2/all')
       .then((res) => res.json())
-      .then((data) => setCountries(data));
+      .then((data) => setCountries(data))
+      .catch((error) => console.log(error));
   }, []);
   return (
     <div className="App">
