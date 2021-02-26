86e6e9aa03a0ac5668878eab4589d94d667acd67
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-27 00:03:09 +0600

    add country name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import logo from './logo.svg';
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -10,26 +9,14 @@ function App() {
       .then((data) => {
         setCountries(data);
         console.log(data);
+        const names = data.map((country) => country.name);
+        console.log(names);
       })
       .catch((error) => console.log(error));
   }, []);
   return (
     <div className="App">
       <h1>country loaded: {countries.length}</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
