b05a57d22e35b520a1ef3487f38ec8bccb6254d1
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-27 18:05:37 +0600

    country count

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Country from './components/Country/Country';
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     fetch('https://restcountries.eu/rest/v2/all')
       .then((res) => res.json())
@@ -16,13 +17,14 @@ function App() {
       .catch((error) => console.log(error));
   }, []);
   const handleAddCountry = (country) => {
-    console.log('added', country);
+    const newCountry = [...cart, country];
+    setCart(newCountry);
   };
 
   return (
     <div className="App">
       <h1>Country Loaded: {countries.length}</h1>
-      <h3>Country Added</h3>
+      <h3>Country Added: {cart.length}</h3>
       {countries.map((country) => (
         <Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code} />
       ))}
