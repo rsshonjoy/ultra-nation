a7dc5b05b132552bfa718ae26d45f11c6d7c3a7d
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-27 18:53:35 +0600

    add population count

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Cart = (props) => {
   const { cart } = props;
+  const totalPopulation = cart.reduce((total, country) => total + country.population, 0);
   return (
     <div>
       <h2>This is cart: {cart.length}</h2>
+      <p>Total population: {totalPopulation}</p>
     </div>
   );
 };
